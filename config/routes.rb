99faa88c723a3bddc3f0dd16b5eaa6b54fec4ca3
Tony <saric.tony@gmail.com>
2017-05-03 05:50:20 -0700

    adds splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'issues#index'
+  root 'sessions#new'
 
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
