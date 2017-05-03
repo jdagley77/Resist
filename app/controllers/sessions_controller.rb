876a406bdb1492462a0c83289c7368ff367c3efa
Tony <saric.tony@gmail.com>
2017-05-03 06:13:26 -0700

    cleaned up indentations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-  user = User.from_omniauth(env["omniauth.auth"])
-  user.access_token = request.env["omniauth.auth"].credentials.token
-  user.refresh_token = request.env["omniauth.auth"].credentials.request_token
-  user.save
-  session[:user_id] = user.id
-  redirect_to "user#show"
+    user = User.from_omniauth(env["omniauth.auth"])
+    user.access_token = request.env["omniauth.auth"].credentials.token
+    user.refresh_token = request.env["omniauth.auth"].credentials.request_token
+    user.save
+    session[:user_id] = user.id
+    redirect_to "user#show"
 
   end
 
   def destroy
-  session[:user_id] = nil
-  redirect_to root_path
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
