ba78f80b7c198ef8e417e736abc2a8d9df9243cd
Tony <saric.tony@gmail.com>
2017-05-02 21:06:05 -0700

    corrects seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,35 +8,25 @@
 
 require 'faker'
 
-User.destroy_all
 Issue.destroy_all
 Comment.destroy_all
 Payment.destroy_all
 
-10.times do
-  User.create({
-    username: Faker::GameOfThrones.character,
-    email: Faker::Internet.email,
-    password: 'password'
-    })
-end
 
 10.times do
-  user = User.all.sample
-  user.issues.create({
+  Issue.create({
     title: Faker::Lorem.sentences(1),
     description: Faker::Lorem.sentences(3),
-    user_id: user.id
+    user_id: 1
     })
 end
 
 10.times do
-  user = User.all.sample
   issue = Issue.all.sample
   issue.payments.create({
     amt: Faker::Number.decimal(2),
     status: 'sent',
     issue_id: issue.id,
-    user_id: user.id
+    user_id: 1
     })
 end
